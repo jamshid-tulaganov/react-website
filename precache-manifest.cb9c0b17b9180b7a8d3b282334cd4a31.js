0cedec5bf30c0da413fbbe2d66de83c915b243f9
jamshid john <79533493+jamshidwizard@users.noreply.github.com>
2022-03-16 07:38:26 +0500

    Updates

---
+++ precache-manifest.cb9c0b17b9180b7a8d3b282334cd4a31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "688016ba16fe7efb3071e8aa569f3dde",
+    "url": "/react-website/index.html"
+  },
+  {
+    "revision": "26c947fa2e1911272f8f",
+    "url": "/react-website/static/css/main.cca13aff.chunk.css"
+  },
+  {
+    "revision": "a8e04fb5e4cf30616317",
+    "url": "/react-website/static/js/2.d4e4e489.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-website/static/js/2.d4e4e489.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26c947fa2e1911272f8f",
+    "url": "/react-website/static/js/main.5a20b835.chunk.js"
+  },
+  {
+    "revision": "ddf2ab572ad5f7b34ed8",
+    "url": "/react-website/static/js/runtime-main.8a49290d.js"
+  }
+]);
